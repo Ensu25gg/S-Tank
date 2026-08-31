f399d4c2d7efa8a64aa62b2b88fec04544502912
Ensu25gg <suugii25gg@gmail.com>
2026-08-31 13:20:48 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,15 @@
 self.addEventListener('fetch', function(event) {});
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('my-game-cache').then((cache) => {
+      return cache.addAll(['/', '/index.html', '/style.css', '/game.js']);
+    })
+  );
+});
